4368184399289f78e1b40207c1012f858a6242a4
MarcosRubi <danielhernandez9980@gmail.com>
2023-06-24 11:06:57 -0600

    Seccion Products Responsive y usando libreria owl-carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    scripts: [
-        { src: './js/lightbox-plus-jquery.min.js' },
-      ],
-      stylesheets: [
-        { href: './css/lightbox.css' },
-      ],
+    
 });
